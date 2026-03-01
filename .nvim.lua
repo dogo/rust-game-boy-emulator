3102392af0f8d9124d462bb7e00927d3c65ef755
Diogo Autilio <diautilio@gmail.com>
2026-03-01 12:56:53 -0300

    fix: escape shell args

--- .nvim.lua
+++ .nvim.lua
@@ -6,7 +6,7 @@ vim.keymap.set("n", "<leader>rr", function()
     args = vim.fn.input("cargo run -- ")
     vim.g.cargo_args = args
   end
-  vim.cmd("split | terminal cargo run -- " .. args)
+  vim.cmd("split | terminal cargo run -- " .. vim.fn.shellescape(args))
 end, { desc = "cargo run com args do projeto" })
 
 vim.keymap.set("n", "<leader>ra", function()
